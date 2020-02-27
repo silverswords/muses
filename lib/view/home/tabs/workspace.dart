37da38c8711194dc040fe7493d499f08efd83ebf
Feng Yifei <fengyfei@gmail.com>
2020-02-27 15:44:19 +0800

    fix: workspace scrollable

--- lib/view/home/tabs/workspace.dart
+++ lib/view/home/tabs/workspace.dart
@@ -3,8 +3,58 @@ import 'package:flutter/material.dart';
 class WorkspaceView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Workspace'),
+    return Container(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+              ), 
+              child: Text('Workspace'),
+            ),
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+              ), 
+              child: Text('Workspace'),
+            ),
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+              ), 
+              child: Text('Workspace'),
+            ),
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+              ), 
+              child: Text('Workspace'),
+            ),
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+              ), 
+              child: Text('Workspace'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
