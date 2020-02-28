b353f5f75997f624e6d272cd60bfc47959d4d7d0
Feng Yifei <fengyfei@gmail.com>
2020-02-28 16:25:13 +0800

    login page

--- lib/view/login/view.dart
+++ lib/view/login/view.dart
@@ -1,21 +1,268 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import 'package:muses/constants/colors.dart';
 import 'package:muses/routing/names.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Visitor Login'),
-          onPressed: () {
-            Navigator.pushNamed(context, HomePageRoute);
-          },
+      body: SafeArea(
+        child: _MainView(
+          usernameController: _usernameController,
+          passwordController: _passwordController,
         ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
+
+class _MainView extends StatelessWidget {
+  const _MainView({
+    Key key,
+    this.usernameController,
+    this.passwordController,
+  }) : super(key: key);
+
+  final TextEditingController usernameController;
+  final TextEditingController passwordController;
+
+  void _login(BuildContext context) {
+    Navigator.pushNamed(context, HomePageRoute);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> listViewChildren;
+
+    final desktopMaxWidth = 400.0 + 100.0;
+    listViewChildren = [
+      _UsernameInput(
+        maxWidth: desktopMaxWidth,
+        usernameController: usernameController,
+      ),
+      const SizedBox(height: 12),
+      _PasswordInput(
+        maxWidth: desktopMaxWidth,
+        passwordController: passwordController,
+      ),
+      _LoginButton(
+        maxWidth: desktopMaxWidth,
+        onTap: () {
+          _login(context);
+        },
+      ),
+    ];
+
+    return Column(
+      children: [
+        Expanded(
+          child: Align(
+            alignment: Alignment.center,
+            child: ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              children: listViewChildren,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _UsernameInput extends StatelessWidget {
+  const _UsernameInput({
+    Key key,
+    this.maxWidth,
+    this.usernameController,
+  }) : super(key: key);
+
+  final double maxWidth;
+  final TextEditingController usernameController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        constraints: BoxConstraints(maxWidth: maxWidth ?? double.infinity),
+        child: TextField(
+          controller: usernameController,
+          decoration: InputDecoration(
+            labelText: 'Username',
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PasswordInput extends StatelessWidget {
+  const _PasswordInput({
+    Key key,
+    this.maxWidth,
+    this.passwordController,
+  }) : super(key: key);
+
+  final double maxWidth;
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        constraints: BoxConstraints(maxWidth: maxWidth ?? double.infinity),
+        child: TextField(
+          controller: passwordController,
+          decoration: InputDecoration(
+            labelText: 'Password',
+          ),
+          obscureText: true,
+        ),
+      ),
+    );
+  }
+}
+
+class _ThumbButton extends StatefulWidget {
+  _ThumbButton({
+    @required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  _ThumbButtonState createState() => _ThumbButtonState();
+}
+
+class _ThumbButtonState extends State<_ThumbButton> {
+  BoxDecoration borderDecoration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      button: true,
+      enabled: true,
+      label: 'Login',
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: Focus(
+          onKey: (node, event) {
+            if (event is RawKeyDownEvent) {
+              if (event.logicalKey == LogicalKeyboardKey.enter ||
+                  event.logicalKey == LogicalKeyboardKey.space) {
+                widget.onTap();
+                return true;
+              }
+            }
+            return false;
+          },
+          onFocusChange: (hasFocus) {
+            if (hasFocus) {
+              setState(() {
+                borderDecoration = BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white.withOpacity(0.5),
+                    width: 2,
+                  ),
+                );
+              });
+            } else {
+              setState(() {
+                borderDecoration = null;
+              });
+            }
+          },
+          child: Container(
+            decoration: borderDecoration,
+            height: 120,
+            child: ExcludeSemantics(
+              child: Image.asset(
+                'thumb.png',
+                package: 'rally_assets',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginButton extends StatelessWidget {
+  const _LoginButton({
+    Key key,
+    @required this.onTap,
+    this.maxWidth,
+  }) : super(key: key);
+
+  final double maxWidth;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        constraints: BoxConstraints(maxWidth: maxWidth ?? double.infinity),
+        padding: const EdgeInsets.symmetric(vertical: 30),
+        child: Row(
+          children: [
+            Icon(Icons.check_circle_outline, color: DefaultColors.buttonColor),
+            const SizedBox(width: 12),
+            Text('RememberMe'),
+            const Expanded(child: SizedBox.shrink()),
+            _FilledButton(
+              text: 'Login',
+              onTap: onTap,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _FilledButton extends StatelessWidget {
+  const _FilledButton({Key key, @required this.text, @required this.onTap})
+      : super(key: key);
+
+  final String text;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      color: DefaultColors.buttonColor,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      onPressed: onTap,
+      child: Row(
+        children: [
+          Icon(Icons.lock),
+          const SizedBox(width: 6),
+          Text(text),
+        ],
       ),
     );
   }
