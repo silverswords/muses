de26906093ca3b40af328934340bfa6f8fa26db1
Feng Yifei <fengyfei@gmail.com>
2020-03-01 00:21:30 +0800

    room page

--- lib/constants/dim.dart
+++
@@ -1,1 +0,0 @@
-const double desktopDisplay1FontDelta = 16;
--- lib/view/room/view.dart
+++ lib/view/room/view.dart
@@ -1,108 +1,54 @@
 import 'package:flutter/material.dart';
 
-import 'package:muses/constants/dim.dart';
 import 'package:muses/routing/names.dart';
 
-const _horizontalPadding = 32.0;
 class RoomPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 8),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: _horizontalPadding),
-          child: _RoomHeader(),
-        ),
-        RoomCard(title: 'Room I', subtitle: 'Javascript'),
-        RoomCard(title: 'Room II', subtitle: 'Go'),
-        RoomCard(title: 'Room III', subtitle: 'Python'),
-        RoomCard(title: 'Room IV', subtitle: 'Julia'),
-      ],
-    );
-  }
-}
-
-class Header extends StatelessWidget {
-  const Header({this.color, this.text});
-
-  final Color color;
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        top: 63,
-        bottom: 21,
-      ),
-      child: Text(
-        text,
-        style: Theme.of(context).textTheme.bodyText1.apply(
-          color: color,
-          fontSizeDelta: desktopDisplay1FontDelta,
-        ),
-      ),
-    );
-  }
-}
-
-class _RoomHeader extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Header(
-      color: Theme.of(context).colorScheme.primary,
-      text: 'Rooms',
-    );
-  }
-}
-
-class RoomCard extends StatelessWidget {
-  const RoomCard({
-    Key key,
-    this.title,
-    this.subtitle,
-  }) : super(key: key);
-
-  final String title;
-  final String subtitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).backgroundColor,
-      width: 400,
-      margin:
-          EdgeInsets.all(10),
-      child: Material(
-        elevation: 4,
-        //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        clipBehavior: Clip.antiAlias,
-        color: Colors.grey,
-        child: InkWell(
-          onTap: () {
-            Navigator.pushNamed(context, HomePageRoute);
-          },
-          child: 
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Theme(
+          data: theme.copyWith(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.red,
+              ),
+            ),
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, HomePageRoute);
+              },
+              child: Row(
+                //mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Text(
-                    title,
-                    maxLines: 3,
-                    overflow: TextOverflow.visible,
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.orange,
+                      child: Center(
+                        child: Text('Rooms')
+                      ),
+                    ),
                   ),
-                  Text(
-                    subtitle,
-                    maxLines: 5,
-                    overflow: TextOverflow.visible,
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.blueAccent,
+                      child: Center(
+                        child: Text('Schedule')
+                      ),
+                    )
                   ),
                 ],
               ),
             ),
+          ),
         ),
       ),
     );
