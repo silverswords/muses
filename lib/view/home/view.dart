2ec0630448d55779a96bf7e135108b6c56dc02af
Feng Yifei <fengyfei@gmail.com>
2020-03-09 14:48:57 +0800

    remove tabs

--- lib/view/home/view.dart
+++ lib/view/home/view.dart
@@ -4,101 +4,15 @@ import 'package:provider/provider.dart';
 import 'package:muses/constants/dim.dart';
 import 'package:muses/models/workspace.dart';
 
-import './widgets/tab_bar.dart';
-import './widgets/tab_item.dart';
-
 import './tabs/workspace.dart';
-import './tabs/setting.dart';
 
 import './widgets/teacher.dart';
 import './widgets/communication.dart';
 
-const int tabCount = 2;
-const int turnsToRotateRight = 1;
-const int turnsToRotateLeft = 3;
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  final tabBarViewWidth = 150.0;
-  TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: tabCount, vsync: this)
-      ..addListener(() {
-        // Set state to make sure that the [_RallyTab] widgets get updated when changing tabs.
-        setState(() {});
-      });
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
-
-    final verticalRotation = turnsToRotateRight;
-    final revertVerticalRotation = turnsToRotateLeft;
-
-    Widget tabViews = RotatedBox(
-      quarterTurns: verticalRotation,
-      child: TabBarView(
-        controller: _tabController,
-        children: _buildTabViews().map(
-          (widget) {
-            // Revert the rotation on the tab views.
-            return RotatedBox(
-              quarterTurns: revertVerticalRotation,
-              child: widget,
-            );
-          },
-        ).toList(),
-      ),
-    );
-    
-    Widget tabBarView = Row(
-      children: [
-        Container(
-          width: tabBarViewWidth,
-          alignment: Alignment.topCenter,
-          padding: const EdgeInsets.symmetric(vertical: 32),
-          child: Column(
-            children: [
-              // Rotate the tab bar, so the animation is vertical for desktops.
-              RotatedBox(
-                quarterTurns: verticalRotation,
-                child: VerticalTabBar(
-                  tabs: _buildTabs(context: context, theme: theme, isVertical: true)
-                      .map((widget) {
-                      // Revert the rotation on the tabs.
-                      return RotatedBox(
-                        quarterTurns: revertVerticalRotation,
-                        child: widget,
-                      );
-                    },
-                  ).toList(),
-                  tabController: _tabController,
-                ),
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          // Rotate the tab views so we can swipe up and down.
-          child: tabViews,
-        ),
-      ],
-    );
 
     return Scaffold(
       body: SafeArea(
@@ -112,7 +26,7 @@ class _HomePageState extends State<HomePage>
           ),
           child: Stack(
             children: [
-              tabBarView,
+              WorkspaceView(),
               _builderTeacherWindow(),
               _builderCommunicationWindow(),
             ],
@@ -122,44 +36,13 @@ class _HomePageState extends State<HomePage>
     );
   }
 
-  List<Widget> _buildTabs(
-      {BuildContext context, ThemeData theme, bool isVertical = false}) {
-    return [
-      TabItem(
-        theme: theme,
-        iconData: Icons.work,
-        title: 'Workspace',
-        tabIndex: 0,
-        tabCount: tabCount,
-        tabController: _tabController,
-        isVertical: isVertical,
-      ),
-      TabItem(
-        theme: theme,
-        iconData: Icons.settings,
-        title: 'Setting',
-        tabIndex: 1,
-        tabCount: tabCount,
-        tabController: _tabController,
-        isVertical: isVertical,
-      ),
-    ];
-  }
-
-  List<Widget> _buildTabViews() {
-    return [
-      WorkspaceView(),
-      SettingView(),
-    ];
-  }
-
   Widget _builderTeacherWindow() {
     return Consumer<WorkspaceStatus>(
       builder: (context, status, _) {
         return Visibility(
           visible: status.teacherViewVisible,
           child: Positioned(
-            left: tabBarViewWidth,
+            left: 0,
             bottom: toolBarHeight,
             child: TeacherView(),
           ),
